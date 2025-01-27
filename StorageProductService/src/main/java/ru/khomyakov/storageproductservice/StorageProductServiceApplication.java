3983430d939339cb77f5a656db06bd0ee6ffa5bc
homval <in00@list.ru>
2025-01-27 17:23:27 +0300

    init commit

---
+++ StorageProductService/src/main/java/ru/khomyakov/storageproductservice/StorageProductServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.khomyakov.storageproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageProductServiceApplication.class, args);
+    }
+
+}
